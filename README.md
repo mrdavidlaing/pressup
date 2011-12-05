2307579f42d44418d83bbafbac501f8c821353c8
David Laing <david@davidlaing.com>
2011-12-05 19:37:02 +0000

    Added test folder for mocha tests

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 is a wordpress install optimised for theme and plugin developers.
 
 #Prerequisits
-sudo npm install -g jake
-sudo npm install wrench
+sudo npm install -g jake mocha
+sudo npm install should 
---
+++ test/BDD_sample_tests.js
@@ -0,0 +1,14 @@
+should = require('should');
+
+describe('Array', function(){
+    before(function(){
+          // ...
+    });
+      
+    describe('#indexOf()', function(){
+      it('should return -1 when not present', function(){
+          [1,2,3].indexOf(4).should.equal(-1);
+      });
+    });
+});
+

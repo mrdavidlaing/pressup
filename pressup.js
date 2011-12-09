d49c7b6dcd95dff2a65a3d94ec713a4126ad147a
David Laing <david@davidlaing.com>
2011-12-09 23:54:50 +0000

    Add build command (no tests yet)

--- pressup.js
+++ pressup.js
@@ -1,5 +1,6 @@
 
 var cli = require('cli'),
+    fs  = require('fs'),
     pressup = {
         init: require('./lib/init'),
         build: require('./lib/build'),
@@ -22,24 +23,24 @@ cli.main(function (args, options) {
       }
 
       if (!options.config) this.fatal('--config is required');
-
       this.info('Using config: ' + options.config);
+      var config = JSON.parse(fs.readFileSync(options.config, 'utf8'));
 
       switch(this.command.toLowerCase()) {
           case 'build':
-            pressup.build(args);
+            pressup.build(args, config);
             break;
           case 'plugin:create':
-            pressup.plugin.create(args);
+            pressup.plugin.create(args, config);
             break;
           case 'theme:create':
-            pressup.theme.create(args);
+            pressup.theme.create(args, config);
             break;
           case 'db:backup':
-            pressup.db.backup(args);
+            pressup.db.backup(args, config);
             break;
           case 'db:restore':
-            pressup.db.restore(args);
+            pressup.db.restore(args, config);
             break;
 
           default:

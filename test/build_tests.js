185b02766082013f3fa375bf70861b3a618e0051
wordpress <wordpress@wordpress-appliance-master.(none)>
2011-12-12 23:39:22 +0000

    Fix file permissions during build

--- test/build_tests.js
+++ test/build_tests.js
@@ -1,6 +1,7 @@
 var should = require('should'),
     fs = require('fs'),
     path = require('path'),
+    global_config = require('../lib/global_config'),
     build = require('../lib/build'),
     sample_repo_folder = 'test/sample_repo',
     util = require('util'),
@@ -8,9 +9,13 @@ var should = require('should'),
 
 describe('build', function(){
     describe('[a new versioned build]\n $ pressup build ' + sample_repo_folder, function(){
-        var result = false, output="";
+        var result = false, output="",
+            file_with_bad_permissions = sample_repo_folder + "/lib/wordpress/wordpress-3.3-RC2/wp-app.php";;
         before(function() {
-            debugger;
+
+            fs.chownSync(file_with_bad_permissions, process.getuid(), process.getgid());  //break ownership
+            fs.chmodSync(file_with_bad_permissions, 0600);  //break permissions
+
             result = build({base_folder: sample_repo_folder},{
                 fqdn: "test.domain.com",
                 version: 42,
@@ -25,6 +30,13 @@ describe('build', function(){
         });
         it('should create a httpdocs/test.domain.com/v42 folder', function(){
           path.existsSync(sample_repo_folder + "/httpdocs/test.domain.com/v42").should.be.true;
+        });
+        it('should fix the file permissions of linked files', function(){
+            var fileStats = fs.statSync(file_with_bad_permissions);
+
+            fileStats.uid.should.equal(global_config.built.uid);
+            fileStats.gid.should.equal(global_config.built.gid);
+            //TODO - how do I check file permissions? fileStats.mode.should.equal(0755);
         });
         it('should set the permissions on the folders correctly', function(){
             fs.statSync(sample_repo_folder + "/httpdocs").isDirectory().should.be.true;

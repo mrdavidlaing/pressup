49e6e026b5f9718c8033e3f1eb21e0d8ede53c98
wordpress <wordpress@wordpress-appliance-master.(none)>
2011-12-13 00:02:36 +0000

    Create wp-config.php during build

--- test/build_tests.js
+++ test/build_tests.js
@@ -31,6 +31,9 @@ describe('build', function(){
         it('should create a httpdocs/test.domain.com/v42 folder', function(){
           path.existsSync(sample_repo_folder + "/httpdocs/test.domain.com/v42").should.be.true;
         });
+        it('should create wp-config.php file', function(){
+          path.existsSync(sample_repo_folder + "/httpdocs/test.domain.com/v42/wp-config.php").should.be.true;
+        });
         it('should fix the file permissions of linked files', function(){
             var fileStats = fs.statSync(file_with_bad_permissions);
 
@@ -41,6 +44,8 @@ describe('build', function(){
         it('should set the permissions on the folders correctly', function(){
             fs.statSync(sample_repo_folder + "/httpdocs").isDirectory().should.be.true;
         });
+
+
     });
     describe('[rebuild of existing version]\n $ pressup build '+ sample_repo_folder + '\n$ pressup build '+ sample_repo_folder , function(){
         before(function(){

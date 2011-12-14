bee68c3154cc63868e20300f5ae0a829619c45a7
wordpress <wordpress@wordpress-appliance-master.(none)>
2011-12-14 19:47:59 +0000

    Copy wordpress rather than symlinking to it

--- test/build_tests.js
+++ test/build_tests.js
@@ -10,11 +10,11 @@ var should = require('should'),
 describe('build', function(){
     describe('[a new versioned build]\n $ pressup build ' + sample_repo_folder, function(){
         var result = false, output="",
-            file_with_bad_permissions = sample_repo_folder + "/lib/wordpress/wordpress-3.3-RC2/wp-app.php";;
+            file_with_bad_permissions = "wp-app.php";
         before(function() {
 
-            fs.chownSync(file_with_bad_permissions, process.getuid(), process.getgid());  //break ownership
-            fs.chmodSync(file_with_bad_permissions, 0600);  //break permissions
+            fs.chownSync(sample_repo_folder + "/lib/wordpress/wordpress-3.3-RC2/" + file_with_bad_permissions, process.getuid(), process.getgid());  //break ownership
+            fs.chmodSync(sample_repo_folder + "/lib/wordpress/wordpress-3.3-RC2/" + file_with_bad_permissions, 0600);  //break permissions
 
             result = build({base_folder: sample_repo_folder},{
                 fqdn: "test.domain.com",
@@ -31,11 +31,15 @@ describe('build', function(){
         it('should create a httpdocs/test.domain.com/v42 folder', function(){
           path.existsSync(sample_repo_folder + "/httpdocs/test.domain.com/v42").should.be.true;
         });
+        it('should copy the wordpress files', function(){
+          path.existsSync(sample_repo_folder + "/httpdocs/test.domain.com/v42/wp-admin").should.be.true;
+          path.existsSync(sample_repo_folder + "/httpdocs/test.domain.com/v42/wp-app.php").should.be.true;
+        });
         it('should create wp-config.php file', function(){
           path.existsSync(sample_repo_folder + "/httpdocs/test.domain.com/v42/wp-config.php").should.be.true;
         });
         it('should fix the file permissions of linked files', function(){
-            var fileStats = fs.statSync(file_with_bad_permissions);
+            var fileStats = fs.statSync(sample_repo_folder + "/httpdocs/test.domain.com/v42/" + file_with_bad_permissions);
 
             fileStats.uid.should.equal(global_config.built.uid);
             fileStats.gid.should.equal(global_config.built.gid);
@@ -43,9 +47,8 @@ describe('build', function(){
         });
         it('should set the permissions on the folders correctly', function(){
             fs.statSync(sample_repo_folder + "/httpdocs").isDirectory().should.be.true;
+            fs.statSync(sample_repo_folder + "/httpdocs/test.domain.com/v42/wp-admin").gid.should.equal(global_config.built.gid);
         });
-
-
     });
     describe('[rebuild of existing version]\n $ pressup build '+ sample_repo_folder + '\n$ pressup build '+ sample_repo_folder , function(){
         before(function(){
--- test/fs_helpers_tests.js
+++ test/fs_helpers_tests.js
@@ -5,6 +5,8 @@ var should = require('should'),
     base_folder = "test/tmp/fs_helpers";
 
 describe('fs_helpers', function(){
+
+
     describe('isSymlinkDir should identify symlinks to other folders', function(){
         before(function() {
             fs_helpers.mkdir(base_folder + "/simple_collection", process.getuid(), process.getgid(), 0765);
@@ -76,6 +78,27 @@ describe('fs_helpers', function(){
 
             fs_helpers.rm(base_folder + "/simple_collection/1");
             fs_helpers.rm(base_folder + "/simple_collection");
+        });
+    });
+
+     describe('copyRecursiveSync should recursively copy everything below a folder', function(){
+       before(function() {
+            fs_helpers.mkdir(base_folder + "/a_collection", process.getuid(), process.getgid(), 0765);
+            fs_helpers.mkdir(base_folder + "/a_collection/1", process.getuid(), process.getgid(), 0765);
+            fs.writeFileSync(base_folder + "/a_collection/1/a_file.txt", "Content");
+            fs_helpers.mkdir(base_folder + "/copy_of_a_folder", process.getuid(), process.getgid(), 0765);
+
+            fs_helpers.copyRecursiveSync(base_folder + "/a_collection", base_folder + "/copy_of_a_folder");
+        });
+
+        it('should copy everything', function(){
+          path.existsSync(base_folder + "/copy_of_a_folder/1").should.be.true;
+          path.existsSync(base_folder + "/copy_of_a_folder/1/a_file.txt").should.be.true;
+        });
+
+        after(function() {
+            fs_helpers.rmdirSyncRecursive(base_folder + "/a_collection");
+            fs_helpers.rmdirSyncRecursive(base_folder + "/copy_of_a_folder");
         });
     });
 });

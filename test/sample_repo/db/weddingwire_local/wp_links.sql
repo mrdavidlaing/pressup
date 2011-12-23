9eae8dd51b413aaf8825210cf3c756868a632787
wordpress <wordpress@wordpress-appliance-master.(none)>
2011-12-23 15:36:37 +0000

    Dump db without extra comments

--- test/sample_repo/db/weddingwire_local/wp_links.sql
+++ test/sample_repo/db/weddingwire_local/wp_links.sql
@@ -1,8 +1,3 @@
--- MySQL dump 10.13  Distrib 5.1.58, for debian-linux-gnu (i686)
---
--- Host: localhost    Database: weddingwire_local
--- ------------------------------------------------------
--- Server version	5.1.58-1ubuntu1-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,11 +9,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `wp_links`
---
-
 DROP TABLE IF EXISTS `wp_links`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -50,4 +40,3 @@ CREATE TABLE `wp_links` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-12-23 15:30:50

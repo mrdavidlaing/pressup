e512af27e205877ae9eb98bf38369efb1db4fb3d
David Laing <david@davidlaing.com>
2012-01-24 00:36:44 +0000

    Experiment with new CLI ui style

--- pressup_new.js
+++ pressup_new.js
@@ -1,62 +1,156 @@
 #!/usr/bin/env node
 
 var program = require('commander'),
+    async = require('async'),
+    color = require('color'),
+    util = require('util'),
+    _ = require('underscore'),
+    current_sites = ['site1', 'site2'],
     get_config = function(callback) {
       if (program.siteConfig) callback(program.siteConfig);
-      var available_configs = ['conf/1.json', 'conf/2.json'];
       console.log("Select the site config:")
-      program.choose(available_configs, function(i) {
-         config = available_configs[i];
+      program.choose(current_sites, function(i) {
+         config = current_sites[i];
          process.stdin.destroy();
          callback(config);
       });   
     };
 
-//Global options
+var validation = {
+  valid_site_name: function(siteName) {
+    if (siteName.length<2) {
+      return { isValid: false, message: "Site name must be longer than 2 characters" };
+    }
+
+    return { isValid: true };
+  }
+};
+
+var prompts = {
+  get_site_name: function(config, callback) {
+    program.prompt('Site name: ', function(newSiteName){
+      var is_valid_site_name = validation.valid_site_name(newSiteName);
+      if (is_valid_site_name.isValid) {
+        config.name = newSiteName;
+        callback(null, config);
+      } else {
+        console.log(is_valid_site_name.message);
+        prompts.get_site_name(config, callback);
+      } 
+    });
+  },
+  select_site_config: function(callback) {
+    console.log("Select existing site:")
+    program.choose(current_sites, function(i) {
+      callback(null, JSON.parse('{"type":"'+current_sites[i]+'"}'));
+    }); 
+  },
+  get_site_to_copy: function(callback) {
+    program.confirm('Copy an existing site? ', function(shouldCopyExistingSite){
+      if (shouldCopyExistingSite) {
+        prompts.select_site_config(callback); 
+      } else {
+        callback(null,{});
+      }
+    });
+  }
+}
+
+console.log("|---------------------------------------------------------|");
+console.log("|  PressUp - automating WordPress development since 2011  |");
+console.log("|---------------------------------------------------------|");
+
 program
-  .version('0.0.1');
+  .version('0.0.1')
+  .on('--help', function(){
+    console.log('  Usage notes:');
+    console.log('');
+    console.log('    You must specify which [command] you want to run.');
+    console.log('    Where a command requires further arguments these can');
+    console.log('    be specified as options, or you will be prompted to');
+    console.log('    answer a series of questions');
+    console.log('');
+  });
+
+//Init 
+program
+  .command('init [site-name]')
+  .description('Initialise a new site configuration (creates config/[site-name].json)')
+  .option('-c, --copy-existing-site <template-site-name>', 'Copy all settings from an existing site')
+  .action(function(siteName, options){
+    async.waterfall([
+        /* copyExistingSite*/ function(callback){
+          if (program.copyExistingSite) {
+            callback(null, JSON.parse('{"type":"the-existing-site-name"}'));
+          } else {
+            prompts.get_site_to_copy(callback);
+          }
+        },
+        /* Site name */ function(config, callback){
+          if (siteName) {
+            config.name = siteName;
+            callback(null, config);
+          } else {
+            prompts.get_site_name(config, callback);
+          }
+        },
+        /* do init */ function(config, callback) {
+          process.stdin.destroy();
+          console.log('Executing: pressup_new.js init %s -c %s', config.name, config.copy_of);
+          console.log('Init new site: '+util.inspect(config));
+        }
+      ],
+      /* handle error */ function(err) {
+          process.stdin.destroy();
+          console.log('An error occured: '+util.inspect(err));
+      });
+    });
 
 //Build command
 program
-  .command('build <env>')
+  .command('build')
   .description('"compile" the specified site config for environment <env>')
-  .option('-s, --site-config <path>', 'set site config path')
-  .action(function(env, options){
-    var env = env || 'dev';
-    get_config(function(config) {
-      console.log('build for %s site for %s env(s)', env, config);
-    });
+  .option('-s, --site <site-name>', 'site to build')
+  .action(function(options){
+    async.waterfall([
+        /* Site name */ function(callback){
+          if (program.site) {
+            config.name = options.site;
+            callback(null, config);
+          } else {
+            prompts.select_site_config(callback);
+          }
+        },
+        /* do build */ function(config, callback) {
+          process.stdin.destroy();
+          console.log('Build site: '+util.inspect(config));
+        }
+      ],
+      /* handle error */ function(err) {
+          process.stdin.destroy();
+          console.log('An error occured: '+util.inspect(err));
+      });
   });
 
-// program
-//   .command('exec <cmd>')
-//   .description('execute the given remote cmd')
-//   .option("-e, --exec_mode <mode>", "Which exec mode to use")
-//   .action(function(cmd, options){
-//     console.log('exec "%s" using %s mode', cmd, options.exec_mode);
-   
-//     var list = ['tobi', 'loki', 'jane', 'manny', 'luna'];
+//DB command
+program
+  .command('db:backup')
+  .description('backup database')
+  .option('-s, --site <site-path>', 'site of database to backup')
+  .action(function(options){
+    console.log("db backup");
+  });
 
-//     console.log('Choose the coolest pet:');
-//     program.choose(list, function(i){
-//       console.log('you chose %d "%s"', i+1, list[i]);
-//       console.log();
-//       console.log('Choose the coolest pet, defaulting to loki:');
-//       program.choose(list, 1, function(i){
-//         console.log('you chose %d "%s"', i+1, list[i]);
-//         process.stdin.destroy();
-//       });
-//     });
-    
-//   }).on('--help', function() {
-//     console.log('  Examples:');
-//     console.log();
-//     console.log('    $ deploy exec sequential');
-//     console.log('    $ deploy exec async');
-//     console.log();
-//   });
+//DB command
+program
+  .command('db:restore')
+  .description('restore database')
+  .option('-t, --target-site <site-name>', 'target site to restore database to')
+  .option('-s, --source-site <site-name>', 'source site to restore database from')
+  .action(function(options){
+    console.log("db restore");
+  });
 
-console.log("------------ PressUp - automating WordPress development since 2011 -----------");
-
+if(process.argv.length === 2) process.argv.push('-h'); //default to showing help when no args
 program.parse(process.argv);
 
